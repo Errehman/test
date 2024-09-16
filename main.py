0b152d89d684aa32812d4ebe17145674bc6e189d
Errehman <eliyevanuride5@gmail.com>
2024-09-16 19:17:55 +0400

    update

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-from flask import Flask, request, jsonify, session
+from flask import Flask, jsonify
 from faker import Faker
 fake = Faker()
 app = Flask(__name__)
 
-app.route("fakedata", methods=['GET'])
+@app.route("/fakedata", methods=['GET'])
 def fakedata():
     data_list = []
     for _ in range(10):
         fake_data = {
-        'name': fake.name(),
-        'address': fake.address(),
-        'text': fake.text()
-    }
+            'name': fake.name(),
+            'address': fake.address(),
+            'text': fake.text()
+        }
         data_list.append(fake_data)
+
     for index, data in enumerate(data_list, start=1):
         print(f"Entry {index}:")
         print(f"Name: {data['name']}")
         print(f"Address: {data['address']}")
         print(f"Text: {data['text']}\n")
-        
+
+    return jsonify(data_list)
+
 if __name__ == '__main__':
     app.run(debug=True)
